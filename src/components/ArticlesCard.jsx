e8ff43ba0004d74edc89aa1e2781857c275db793
SimoneDessi <simonedessi1984@gmail.com>
2023-07-18 11:49:17 +0200

    will render a list of all article, basic css that works with mobile and loading functionality

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -1,11 +1,10 @@
-const ArticlesCard = ({title, author, topic}) => {
+const ArticlesCard = ({ title, author, topic }) => {
   return (
-    <div className="card">
-       <h2 className="h2">{title}</h2>
-    <p>Author: {author}</p>
-    <p>Topic: {topic}</p>
-
-       </div>
-  )
-}
+    <div>
+      <h2>{title}</h2>
+      <p>Author: {author}</p>
+      <p>Topic: {topic}</p>
+    </div>
+  );
+};
 export default ArticlesCard;
--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,44 +1,41 @@
 import { useState, useEffect } from "react";
-import ArticlesCard from "../components/ArticlesCard"
+import ArticlesCard from "../components/ArticlesCard";
 import { getAllArticles } from "../Api";
-import Newspaper from "../images/Newspaper.jpeg"
+import Newspaper from "../images/Newspaper.jpeg";
 
 const Articles = () => {
   const [articles, setArticles] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     getAllArticles().then((articlesFromApi) => {
       setArticles(articlesFromApi);
+      setLoading(false);
     });
   }, []);
-return(<>
-  <h1 className="article_header">All articles</h1>
-  <img src={Newspaper} alt="newspaper image" className="article_image"/>
-<div className="articles-container">
-  <ol className="article_list">
-        {articles.map(
-          ({
-            article_id,
-            author,
-            title,
-            topic,
-          
-          }) => {
-            return (
-              <ArticlesCard
-                key={article_id}
-                title={title}
-                author={author}
-                topic={topic}
-              />
-            );
-          }
+  return (
+    <>
+      <h1 className="article_header">All articles available </h1>
+      <img src={Newspaper} alt="newspaper image" className="article_image" />
+      <div className="articles-container">
+        {loading ? (
+          <p> Page is loading...</p>
+        ) : (
+          <ol className="article_list">
+            {articles.map(({ article_id, author, title, topic }) => {
+              return (
+                <ArticlesCard
+                  key={article_id}
+                  title={title}
+                  author={author}
+                  topic={topic}
+                />
+              );
+            })}
+          </ol>
         )}
-      </ol>
       </div>
-      </>
-)
+    </>
+  );
+};
 
-}
-
-export default Articles
+export default Articles;
