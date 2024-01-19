1547011aa620e75cc7484677a3da367ba0dd9ced
SimoneDessi <simonedessi1984@gmail.com>
2024-01-19 18:23:08 +0000

    will add banner without styling on compnennts folder

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+    <div className="banner-container">
+      <div className="item1"></div>
+      <div className="item2"></div>
+      <div className="item3"></div>
+      <div className="item4"></div>
+      <div className="item5"></div>
+      <div className="item6"></div>
+      <div className="item7"></div>
+    </div>
+  )
+}
